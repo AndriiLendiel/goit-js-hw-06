3fc030656976d466821e4ec4cd9858f6bfcfe8e4
AndriiLendiel <104030684+AndriiLendiel@users.noreply.github.com>
2022-09-01 09:43:45 +0300

    Delete src directory

--- src/js/task-07.js
+++
@@ -1,8 +0,0 @@
-const fsControl = document.querySelector('#font-size-control');
-const textSpan = document.querySelector('#text');
-
-fsControl.addEventListener('input', changeSize)
-
-function changeSize(event) {
-    textSpan.style = `font-size: ${event.target.value}px`;
-};
